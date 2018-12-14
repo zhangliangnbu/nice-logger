92f44d60e3e10b464efe6cef373ab675ae15ab78
zhangliang <zhangliangnbu@163.com>
2018-12-14 09:22:32 +0800

    init

---
+++ nicelogger/src/main/java/com/liang/nicelogger/LoggerUtils.kt
@@ -0,0 +1,13 @@
+package com.liang.nicelogger
+
+import android.util.Log
+
+/**
+ * created by zhangliang on 2018/12/13
+ * profile: zhangliangnbu@163.com
+ */
+object LoggerUtils {
+    fun d(message: String) {
+        Log.d("nice", message)
+    }
+}
